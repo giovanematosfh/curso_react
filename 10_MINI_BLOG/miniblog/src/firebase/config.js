8c74b2d8b3f96824810b0ec4184b502a6cb01640
Giovane Matos <filho1987@gmail.com>
2022-09-21 22:21:14 -0300

    first commit

---
+++ 10_MINI_BLOG/miniblog/src/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjAgmihY6Bq3BkJchw4n4tJNgbqnjGcEk",
+  authDomain: "miniblog-d3bac.firebaseapp.com",
+  projectId: "miniblog-d3bac",
+  storageBucket: "miniblog-d3bac.appspot.com",
+  messagingSenderId: "852665582458",
+  appId: "1:852665582458:web:4125530951225ef9630cc3"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export {db};
